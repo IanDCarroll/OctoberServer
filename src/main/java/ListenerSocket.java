228ee808d4821ec18e2b1aecdab4bcb2cf9a779a
Ian Carroll <ian.d.carroll@gmail.com>
2017-11-06 16:08:00 -0800

    return 404 to a single request when server starts

--- src/main/java/ListenerSocket.java
+++ src/main/java/ListenerSocket.java
@@ -1,17 +1,36 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.SocketException;
 
 public class ListenerSocket {
-    // on init takes a Client Socket as a param
     // implements Reactive Programming to manage Server Streams and concurrency
-    // on .accept initializes a new client socket
-    public ResponderSocket client;
+    private ResponderSocket responder;
+    private int port;
 
-    public ListenerSocket(int port, String directory, ResponderSocket client) {
-        this.client = client;
+    public ListenerSocket(int port, String directory, ResponderSocket responder) {
+        this.port = port;
+        this.responder = responder;
     }
 
     public void start() {
         System.out.println("starting server");
+        try {
+            toStartListening();
+        } catch (IOException e) {
+            System.out.println("IOException while trying to initialize Server Socket");
+        }
+    }
+
+    private void toStartListening() throws IOException {
+        ServerSocket listener = new ServerSocket(port);
+        try {
+            toAcceptAnIncomingCall(listener);
+        } catch (SocketException e) {
+            System.out.println("SocketException while trying to listen for requests");
+        }
+    }
+
+    private void toAcceptAnIncomingCall(ServerSocket listener) throws IOException {
+        responder.respondTo(listener.accept());
     }
 }
--- src/main/java/ResponderSocket.java
+++ src/main/java/ResponderSocket.java
@@ -1,3 +1,9 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class ResponderSocket {
     // on init accepts a controller as a parameter
     // accepts a byte[] from a client
@@ -6,6 +12,10 @@ public class ResponderSocket {
     public Parser parser;
     public Controller controller;
     public DeParser deParser;
+    public Socket clientConnection;
+    public BufferedInputStream listener;
+    public BufferedOutputStream responder;
+    public final int MAX_REQUEST_SIZE = 1000;
 
 
     public ResponderSocket(ByteConverter byteConverter, Parser parser, Controller controller, DeParser deParser) {
@@ -14,4 +24,55 @@ public class ResponderSocket {
         this.controller = controller;
         this.deParser = deParser;
     }
+
+    public void respondTo(Socket clientConnection) {
+        this.clientConnection = clientConnection;
+        try {
+           toSendAResponse();
+        } catch (IOException e) {
+            System.out.println("IOException caught while trying to send a response to the client connection");
+        }
+        thenCloseTheConnection();
+    }
+     private void toSendAResponse() throws IOException {
+         this.listener = new BufferedInputStream(clientConnection.getInputStream());
+         this.responder = new BufferedOutputStream(clientConnection.getOutputStream());
+         try {
+             toComposeSomethingMoreThanNothing();
+         } catch (IOException e) {
+             System.out.println("IOException caught while trying compose something more than nothing"); }
+     }
+
+     private void toComposeSomethingMoreThanNothing() throws IOException {
+        try {
+            toWriteAResponse();
+        } catch (NullPointerException e) {
+            System.out.println("Null Pointer encountered while trying to compose something more than nothing");
+        }
+     }
+
+     private void toWriteAResponse() throws NullPointerException, IOException {
+        String response = readRequest();
+        System.out.println(response);
+        byte[] responsePayload = "HTTP/1.1 404 Not Found\r\n\r\n".getBytes(); // This is the gateway to the functional core
+        responder.write(responsePayload);
+        responder.flush();
+     }
+
+     private String readRequest() throws NullPointerException, IOException {
+         byte[] request = new byte[MAX_REQUEST_SIZE];
+         int count;
+         while((count = listener.read(request)) > 0) {
+             return new String(request);
+         }
+         return new String(request);
+     }
+
+     private void thenCloseTheConnection() {
+        try {
+            clientConnection.close();
+        } catch (IOException e) {
+            System.out.println("IOException caught while trying to close the client connection");
+        }
+     }
 }
