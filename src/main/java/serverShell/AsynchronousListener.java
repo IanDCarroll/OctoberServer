0fba105bcf479c9e7663dac395ee065131d0f692
Ian Carroll <ian.d.carroll@gmail.com>
2017-11-22 17:03:45 -0800

    keep listener's thread alive when working asynchronously

--- src/main/java/serverShell/AsynchronousListener.java
+++ src/main/java/serverShell/AsynchronousListener.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class AsynchronousListener {
     private final AsynchronousServerSocketChannel listener;
@@ -18,5 +19,14 @@ public class AsynchronousListener {
     public void listen(AsynchronousHandler handler) {
         Void noAttachment = null;
         listener.accept(noAttachment, handler);
+        keepTheThreadAlive();
+    }
+
+    private void keepTheThreadAlive() {
+        long thisMuchTime = Long.MAX_VALUE;
+        TimeUnit byThisMeasure = TimeUnit.DAYS; // about 252 trillion years with long's max value
+        try {
+            threadpool.awaitTermination(thisMuchTime, byThisMeasure);
+        } catch (InterruptedException e) { System.out.println("Thread interrupted."); }
     }
 }
