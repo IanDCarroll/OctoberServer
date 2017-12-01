90dcf1005ba4c6af3edc9f40a3b2e1b6b320c755
Ian Carroll <ian.d.carroll@gmail.com>
2017-12-01 15:13:17 -0800

    rename formatter.format to something more descriptive

--- src/main/java/Loggers/ConsoleLogger.java
+++ src/main/java/Loggers/ConsoleLogger.java
@@ -3,6 +3,6 @@ package Loggers;
 public class ConsoleLogger implements Logger {
     @Override
     public void log(String report) {
-        System.out.println(Formatter.format(report));
+        System.out.println(Formatter.stampTime(report));
     }
 }
--- src/main/java/Loggers/Formatter.java
+++ src/main/java/Loggers/Formatter.java
@@ -4,7 +4,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Formatter {
-    public static String format(String report) {
+    public static String stampTime(String report) {
         String sp = "\n";
         String humanDate = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         return humanDate + sp + report + sp;
