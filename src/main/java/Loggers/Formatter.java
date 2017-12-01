3df2551e11f39568dc671b2a9c29a9c000e13604
Ian Carroll <ian.d.carroll@gmail.com>
2017-12-01 15:50:45 -0800

    add tests and refactor Formatter

--- src/main/java/Loggers/Formatter.java
+++ src/main/java/Loggers/Formatter.java
@@ -5,8 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public class Formatter {
     public static String stampTime(String report) {
+        String timeStamp = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        return stamp(report, timeStamp);
+    }
+
+    public static String stamp(String report, String stamp) {
         String sp = "\n";
-        String humanDate = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        return humanDate + sp + report + sp;
+        return stamp + sp + report + sp;
     }
 }
---
+++ src/test/java/LoggerTests/FormatterTest.java
@@ -0,0 +1,21 @@
+package LoggerTests;
+
+import Loggers.Formatter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormatterTest {
+    @Test
+    void stampAddsAStringOfMetaDataToTheLogEntry() {
+        //Given
+        String entry = "This is totally some report from the system";
+        String stamp = "This is totally a stamp of metaData to add to the entry";
+        //When
+        String actual = Formatter.stamp(entry, stamp);
+        //Then
+        String expected = stamp + "\n" + entry + "\n";
+        assertEquals(expected, actual);
+    }
+
+}
