3c4352bb0716f79231981a0ef20197bc0f51130b
Ian Carroll <ian.d.carroll@gmail.com>
2017-12-04 13:38:04 -0800

    add Request Object as a means of storing a parsed request

--- src/main/java/FunctionalCore/Request.java
+++ src/main/java/FunctionalCore/Request.java
@@ -1,11 +1,32 @@
 package FunctionalCore;
 
+import java.util.LinkedList;
+
 public class Request {
-    // request object
-    // Has individual properties representing each element of a request
-    public Request() {
-    }
+    private static final String notSet = "Not Set";
+    private String method = notSet;
+    private String uri = notSet;
+    private LinkedList<String> uriParams = new LinkedList<>();
+    private String httpV = notSet;
+    private LinkedList<String> headers = new LinkedList<>();
+    private byte[] body = notSet.getBytes();
 
-    public boolean exists() { return true; }
 
+    public void setMethod(String method) { this.method = method; }
+    public String getMethod() { return method; }
+
+    public void setUri(String uri) { this.uri = uri; }
+    public String getUri() { return uri; }
+
+    public void addUriParam(String uriParam) { uriParams.add(uriParam); }
+    public String[] getUriParams() { return uriParams.toArray(new String[uriParams.size()]); }
+
+    public void setHttpV(String httpV) { this.httpV = httpV; }
+    public String getHttpV() { return httpV; }
+
+    public void addHeader(String header) { headers.add(header); }
+    public String[] getHeaders() { return headers.toArray(new String[headers.size()]); }
+
+    public void setBody(byte[] body) { this.body = body; }
+    public byte[] getBody() { return body; }
 }
--- src/test/java/FunctionalCoreTests/RequestTest.java
+++ src/test/java/FunctionalCoreTests/RequestTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RequestTest {
     public Request subject;
+    public final String notSet = "Not Set";
 
     @BeforeEach
     void init() {
@@ -15,9 +16,140 @@ class RequestTest {
     }
 
     @Test
-    void sanityTest() {
-        boolean actual = subject.exists();
-        boolean expected = true;
-        assertEquals(expected, actual);
+    void setMethodSetsTheHttpMethod() {
+        //Given
+        String method = "GET";
+        //When
+        subject.setMethod(method);
+        //Then
+        assertEquals(method, subject.getMethod());
+    }
+
+    @Test
+    void setUriSetsTheUri() {
+        //Given
+        String uri = "/any-arbitrary-string";
+        //When
+        subject.setUri(uri);
+        //Then
+        assertEquals(uri, subject.getUri());
+    }
+
+    @Test
+    void setHttpVSetsTheHttpV() {
+        //Given
+        String httpV = "HTTP/1.1";
+        //When
+        subject.setHttpV(httpV);
+        //Then
+        assertEquals(httpV, subject.getHttpV());
+    }
+
+    @Test
+    void setBodySetsTheBody() {
+        //Given
+        byte[] body = "Any Arbitrary Array of Bytes".getBytes();
+        //When
+        subject.setBody(body);
+        //Then
+        assertArrayEquals(body, subject.getBody());
+    }
+
+    @Test
+    void addUriParamAddsOneUriParamTotheLinkedListOfParams() {
+        //Given
+        String[] expected = { "A string representing an arbitrary uriParam" };
+        //When
+        subject.addUriParam(expected[0]);
+        //Then
+        assertArrayEquals(expected, subject.getUriParams());
+    }
+
+    @Test
+    void addUriParamCanBeCalledAnArbitraryNumberOfTimesToPopulateTheLinkedListOfParams() {
+        //Given
+        String[] expected = { "param1", "param2", "param3" };
+        //When
+        subject.addUriParam(expected[0]);
+        subject.addUriParam(expected[1]);
+        subject.addUriParam(expected[2]);
+        //Then
+        assertArrayEquals(expected, subject.getUriParams());
+    }
+
+    @Test
+    void addHeaderAddsOneHeaderTotheLinkedListOfHeaders() {
+        //Given
+        String[] expected = { "A string representing an arbitrary header" };
+        //When
+        subject.addHeader(expected[0]);
+        //Then
+        assertArrayEquals(expected, subject.getHeaders());
+    }
+
+    @Test
+    void addHeaderCanBeCalledAnArbitraryNumberOfTimesToPopulateTheLinkedListOfHeaders() {
+        //Given
+        String[] expected = { "header1", "header2", "header3" };
+        //When
+        subject.addHeader(expected[0]);
+        subject.addHeader(expected[1]);
+        subject.addHeader(expected[2]);
+        //Then
+        assertArrayEquals(expected, subject.getHeaders());
+    }
+
+    @Test
+    void getMethodGetsANotSetMessageIfItHasntBeenSet() {
+        //When
+        String actual = subject.getMethod();
+        //Then
+        assertEquals(notSet, actual);
+    }
+
+    @Test
+    void getUriGetsANotSetMessageIfItHasntBeenSet() {
+        //When
+        String actual = subject.getUri();
+        //Then
+        assertEquals(notSet, actual);
+    }
+
+    @Test
+    void getUriParamsReturnsAnEmptyStringArrayIfNoParamsWereAdded() {
+        //Given
+        String[] expected = {};
+        //When
+        String[] actual = subject.getUriParams();
+        //Then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void getHttpVGetsANotSetMessageIfItHasntBeenSet() {
+        //When
+        String actual = subject.getHttpV();
+        //Then
+        assertEquals(notSet, actual);
+    }
+
+    @Test
+    void getHeadersReturnsAnEmptyStringArrayIfNoHeadersWereAdded() {
+        //Given
+        String[] expected = {};
+        //When
+        String[] actual = subject.getHeaders();
+        //Then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void getBodyGetsANotSetMessageIfItHasntBeenSet() {
+        //Given
+        byte[] expected = notSet.getBytes();
+        //When
+        byte[] actual = subject.getBody();
+        //Then
+        assertArrayEquals(expected, actual);
     }
 }
