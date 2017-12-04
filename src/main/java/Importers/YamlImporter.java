23d25100ad27d103474fe435a72000840bfe0421
Ian Carroll <ian.d.carroll@gmail.com>
2017-12-04 10:45:14 -0800

    refactor YamlImporter

--- src/main/java/Importers/YamlImporter.java
+++ src/main/java/Importers/YamlImporter.java
@@ -9,18 +9,14 @@ import java.io.InputStream;
 import java.util.LinkedHashMap;
 
 public class YamlImporter implements Importers.FileImporter {
+    private Yaml yaml = new Yaml();
+    private InputStream input = null;
 
     @Override
     public LinkedHashMap importAsHash(String filePath) {
-
-        Yaml yaml = new Yaml();
-
-        InputStream input = null;
         try {
             input = new FileInputStream(new File(filePath));
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+        } catch (FileNotFoundException e) { System.out.println(e.getMessage()); }
         return yaml.load(input);
     }
 }
