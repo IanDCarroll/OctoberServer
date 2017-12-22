360d3ce2c93623caac9ffd42722cc316e83a3d7f
Ian Carroll <ian.d.carroll@gmail.com>
2017-12-22 11:33:55 -0800

    refactor ResponseGenerator to use an enum

--- src/main/java/FunctionalCore/Controller/ResponseGeneration/ResponseGenerator.java
+++ src/main/java/FunctionalCore/Controller/ResponseGeneration/ResponseGenerator.java
@@ -6,38 +6,53 @@ import java.util.Arrays;
 
 public class ResponseGenerator {
     FileClerk fileClerk;
+    public enum Code {
+        OK                      (new String[]{"200", "OK"}),
+        PARTIAL_CONTENT         (new String[]{"206", "Partial Content"}),
+        FOUND                   (new String[]{"302", "Found"}),
+        UNAUTHORIZED            (new String[]{"401", "Unauthorized"}),
+        FORBIDDEN               (new String[]{"403", "Forbidden"}),
+        NOT_FOUND               (new String[]{"404", "Not Found"}),
+        METHOD_NOT_ALLOWED      (new String[]{"405", "Method Not Allowed"}),
+        RANGE_NOT_SATISFIABLE   (new String[]{"416", "Range Not Satisfiable"}),
+        IM_A_TEAPOT             (new String[]{"418", "I'm a teapot"});
+        public String[] tuple;
+        Code(String[] tuple) {
+            this.tuple = tuple;
+        }
+
+    }
 
     public ResponseGenerator(FileClerk fileClerk) {
         this.fileClerk = fileClerk;
     }
 
-    private final String[] ok200 = { "200", "OK" };
     Response response;
 
     public byte[] generate200(String uri, String[] params) {
         response = new Response();
-        setResponseStartLine(ok200);
+        setResponseStartLine(Code.OK.tuple);
         setParamsWithBody(uri, params);
         return response.getResponse();
     }
 
     public byte[] generate200Head(String uri) {
         response = new Response();
-        setResponseStartLine(ok200);
+        setResponseStartLine(Code.OK.tuple);
         setBody(uri);
         return response.getHead();
     }
 
     public byte[] generate200Options(String permittedMethods) {
         response = new Response();
-        setResponseStartLine(ok200);
+        setResponseStartLine(Code.OK.tuple);
         response.setHeader(Response.Header.ALLOW, permittedMethods);
         return response.getHead();
     }
 
     public byte[] generate206(String uri, int start, int end) {
         response = new Response();
-        setResponseStartLine(new String[]{"206", "Partial Content"});
+        setResponseStartLine(Code.PARTIAL_CONTENT.tuple);
         setBody(uri, start, end);
         response.setHeader(Response.Header.CONTENT_RANGE, rangeValue(uri, start, end));
         return response.getResponse();
@@ -45,49 +60,48 @@ public class ResponseGenerator {
 
     public byte[] generate302(String redirectToThisUri) {
         response = new Response();
-        setResponseStartLine(new String[]{"302", "Found"});
+        setResponseStartLine(Code.FOUND.tuple);
         response.setHeader(Response.Header.LOCATION, redirectToThisUri);
         return response.getHead();
     }
 
     public byte[] generate401() {
         response = new Response();
-        setResponseStartLine(new String[]{"401", "Unauthorized"});
+        setResponseStartLine(Code.UNAUTHORIZED.tuple);
         response.setHeader(Response.Header.WWW_AUTHENTICATE, "Basic realm=\"Access to URI\"");
         return response.getHead();
     }
 
     public byte[] generate403() {
         response = new Response();
-        setResponseStartLine(new String[]{"403", "Forbidden"});
+        setResponseStartLine(Code.FORBIDDEN.tuple);
         return response.getHead();
     }
 
     public byte[] generate404() {
         response = new Response();
-        setResponseStartLine(new String[]{ "404", "Not Found" });
+        setResponseStartLine(Code.NOT_FOUND.tuple);
         return response.getHead();
     }
 
     public byte[] generate405(String permittedMethods) {
         response = new Response();
-        setResponseStartLine(new String[]{ "405", "Method Not Allowed" });
+        setResponseStartLine(Code.METHOD_NOT_ALLOWED.tuple);
         response.setHeader(Response.Header.ALLOW, permittedMethods);
         return response.getHead();
     }
 
     public byte[] generate416(String uri) {
         response = new Response();
-        setResponseStartLine(new String[]{ "416", "Range Not Satisfiable" });
+        setResponseStartLine(Code.RANGE_NOT_SATISFIABLE.tuple);
         response.setHeader(Response.Header.CONTENT_RANGE, rangeValue(uri));
         return response.getHead();
     }
 
     public byte[] generate418() {
         response = new Response();
-        String message = "I'm a teapot";
-        setResponseStartLine(new String[]{ "418", message });
-        setBody(message.getBytes());
+        setResponseStartLine(Code.IM_A_TEAPOT.tuple);
+        setBody(Code.IM_A_TEAPOT.tuple[1].getBytes());
         return response.getResponse();
     }
 
